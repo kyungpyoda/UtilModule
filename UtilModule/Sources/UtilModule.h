83a632f936a87f1a90a90a95f8ff934a2424d760
홍경표 <qorghdl@naver.com>
2021-06-28 15:38:34 +0900

    Init Dynamic Framework Project
    - iOS 13.0+

---
+++ UtilModule/Sources/UtilModule.h
@@ -0,0 +1,20 @@
+//
+//  UtilModule.h
+//  UtilModule
+//
+//  Created by 홍경표 on 2021/06/28.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtilModule.
+FOUNDATION_EXPORT double UtilModuleVersionNumber;
+
+//! Project version string for UtilModule.
+FOUNDATION_EXPORT const unsigned char UtilModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtilModule/PublicHeader.h>
+
+
